0546c0b743ed62e0dd100d4dcf126ec04914286b
Ed Shell <shelleg@g.cofc.edu>
2018-06-12 13:33:37 -0400

    java file

---
+++ app.js
@@ -0,0 +1,12 @@
+// Var query
+
+var queryUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?" + apiKey + "&q=" + searchTerm 
+var apiKey = "api-key=8de9135e72bd4a4ca79255deee6b17bf&q"
+var searchTerm;
+
+$("#searchTerm").click(function(event) {
+    
+    event.preventDefault();
+
+    searchTerm = 
+})
